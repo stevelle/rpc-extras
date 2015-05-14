a5aafb56c692329e7cc13d9da9a6aae537d3e4f5
Nolan Brubaker <nolan.brubaker@rackspace.com>
2015-05-14 15:49:25 -0400

    Fix some configuration file management issues.
    v11+ should put configuration in /etc/openstack_deploy.
    
    dynamic_inventory doesn't generate the inventory file itself, ansible
    does, so we need to make sure the file's present.
    
    The os-a-d upgrade script is run-upgrade.sh, not do_upgrade.sh
    
    (cherry picked from commit 34934859c19b9918e5dc6449c3afe95ebe42b764)

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -27,14 +27,19 @@ if [[ -f "${RPC_CONFIG_FILE}" ]]; then
     ./make_repo_stanza.py "${RPC_CONFIG_FILE}" >> "${RPC_CONFIG_FILE}"
 fi
 
-# generate a new inventory so the load balancers know about the repo containers.
-"${OSAD_DIR}"/playbooks/inventory/dynamic_inventory.py
+# Create an openstack_deploy directory if there isn't already one.
+# Prior to 11.x, it used rpc_deploy.
+if [[ ! -d /etc/openstack_deploy ]]; then
+    mkdir /etc/openstack_deploy
+fi
 
+# generate a new inventory so the load balancers know about the repo containers.
+"${OSAD_DIR}"/playbooks/inventory/dynamic_inventory.py  >> /etc/openstack_deploy/openstack_inventory.json
 
 # Do the actual upgrade
-/opt/rpc-extras/os-ansible-deployment/scripts/do_upgrade.sh
+/opt/rpc-extras/os-ansible-deployment/scripts/run-upgrade.sh
 
-# install RPC
+# install RPC-specific stuff
 source /opt/rpc-extras/os-ansible-deployment/scripts/scripts-library.sh
 cd "${RPCD_DIR}"/playbooks/
 
